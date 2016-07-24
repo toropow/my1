e5755658d5110c3fc7b41577c0bd09262ed42f70
Toropow <toropow@gmail.com>
2016-07-24 08:15:15 +0400

    hello

---
+++ src/first.java
@@ -0,0 +1,10 @@
+/**
+ * Created by семья on 23.07.2016.
+ */
+public class first {
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+        System.out.println("hi");
+    }
+}
